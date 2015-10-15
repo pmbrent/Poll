8940f6e2ea1c361b9cc45f7c3e21604d2bdff996
Mike and Pat <patandmichael@appacademy.io>
2015-10-15 11:39:25 -0400

    Add custom validations to Response.

--- app/models/response.rb
+++ app/models/response.rb
@@ -2,6 +2,8 @@ class Response < ActiveRecord::Base
 
   validates :answer_id, presence: true
   validates :user_id, presence: true
+  validate :respondent_has_not_already_answered_question
+  validate :respondent_is_not_author
 
   belongs_to(
     :answer_choice,
@@ -24,9 +26,22 @@ class Response < ActiveRecord::Base
   )
 
   def sibling_responses
+    # responses.where.not(id: self.id)
+    where_str = self.persisted? ? "responses.id != ?" : "responses.id IS NOT NULL"
+    self.question.responses.where(where_str, self.id)
+  end
 
-    self.question.responses.where(responses.id != self.id)
+private
+  def respondent_has_not_already_answered_question
+    if sibling_responses.exists?(user_id: self.user_id)
+      errors.add(:response, "user already answered this question")
+    end
+  end
 
+  def respondent_is_not_author
+    if self.answer_choice.question.poll.author_id == self.user_id
+      errors.add(:response, "respondent is the author!")
+    end
   end
 
 end
